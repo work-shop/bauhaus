afc2795ae01d3893d781f9ac13b4fbfc8bcf0929
Greg Nemes <mail@gregnemes.com>
2015-05-31 08:10:41 -0400

    connecting javascript files

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,23 +5,15 @@ var loaded = false;
 var state = 'intro';
 var moving = false;
 
-
+console.log('functions.js');
 
 //initial events, and general event binding
 jQuery(document).ready(function($) {
 
 	view();
 
-	videoSetup();
-
 	/* for touch scrolling, this event fires when touch point is moved*/
 	document.addEventListener("touchmove", scrollStart, false);
-
-	$('#fade').click(function(event) {
-	  	event.preventDefault();
-		console.log('fade');
-		$(this).addClass('fade-out');
-	});
 	
 	$('#backtotop').click(function(event) {
 	  	event.preventDefault();
@@ -40,43 +32,9 @@ jQuery(document).ready(function($) {
 		href = href.toLowerCase();
 		scrollLink(href);	
 	});
-	
-	$('.single .story-tile').click(function(event) {
-	  	event.preventDefault();
-		storyToggle($(this));
-	});		
-
-	$('.story-toggle').click(function(event) {
-	  	event.preventDefault();
-		storyToggle($(this));
-	});	
-
-	storySetup();
-
-	loadInitialStory();			
+		
 
 });//end document.ready
-
-function storySetup() {
-	$(window).ready(function() { $('[data-toggle="tooltip"]').tooltip(); });//end window.ready
-
-	$('.story-video-play').click(function(event) {
-	  	event.preventDefault();
-		playVideo();
-	});
-
-	$('.flexslider .flex-previous').click(function() {
-	    $('.flexslider').flexslider('prev');
-	    return false;		
-	});		
-	
-	$('.flexslider .flex-next').click(function() {
-	    $('.flexslider').flexslider('next');
-	    return false;		
-	});	
-
-}
-
 
 
 $(window).resize(function() {
@@ -91,14 +49,15 @@ $(window).resize(function() {
 
 //m or M	
 $(document).keypress(function(e) {
+
 	if(e.which == 109 || e.which == 77) {	
+						console.log('MS');	
+
 		if($("input:focus")){
 			var elem = document.activeElement;
-			if ( elem.type ){ 
-				
-			}
-			else{ 
-				menuToggle();	
+			if (! elem.type ){ 
+				menuToggle();
+				console.log('MS');	
 			}
 		}
 	}
@@ -106,29 +65,17 @@ $(document).keypress(function(e) {
 
 //down arrow
 $(document).keydown(function(e){
-    if (e.keyCode == 40 && !moving) { 
-    	moving = true;
-    	pos = stateNext.offset().top;
-
-	 	$('html,body').animate({
-			scrollTop: pos
-		},2000, function() {
-			moving = false;
-		});  
-       return false;
+    if (e.keyCode == 40) { 
+		
+	}  
+    return false;
     }
 });
 
 //up arrow
 $(document).keydown(function(e){
-    if (e.keyCode == 38 && !moving) { 
-    	moving = true;
-    	pos = statePrevious.offset().top;
-
-	 	$('html,body').animate({
-			scrollTop: pos
-		},2000, function() {
-			moving = false;
+    if (e.keyCode) { 
+    
 		});  
        return false;
     }
@@ -185,170 +132,22 @@ function scrollLink(destination){
 function menuToggle(){
 	console.log('menutoggle');
 	
-	if($('#menu').hasClass('off')){
-		$('#menu').removeClass('off');
-		$('#menu').addClass('on');
+	if($('html').hasClass('menu-closed')){
+		$('#menu').removeClass('closed');
+		$('#menu').addClass('open');
 		$('html').removeClass('menu-closed');
 		$('html').addClass('menu-open');
-		$('#menu').scrollTop(0);				
-		$('html,body').scrollTop(0);			
-		var trim = $(window).height();		
-		$('html,body').css('height',trim);
-		$('html,body').css('overflow','hidden');
 	}
 	
-	else if($('#menu').hasClass('on')){
-		$('#menu').removeClass('on');
-		$('#menu').addClass('off');
+	else if($('html').hasClass('menu-open')){
+		$('#menu').removeClass('open');
+		$('#menu').addClass('closed');
 		$('html').removeClass('menu-open');
 		$('html').addClass('menu-closed');
-		$('html').scrollTop(0);	
-		$('html,body').css('height','100%');
-		$('html,body').css('overflow','visible');
 	}
 	
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**
- * storyToggle : jQuery(clicked-tile) -> ()
- */
-function storyToggle(clicked){
-
-	//console.log( 'startToggle' );
-	//console.log( clicked );
-
-	ch = $(window).height();
-	cw = $(window).width();
-	heroHeight = cw/3;
-	storyHeight = ch - 110;
-
-	if($('#active-story').hasClass('story-loaded')){
-		$('body').removeClass('story-loaded').removeClass('story-video-active').addClass('story-loading');
-		$('#active-story').removeClass('story-loaded').addClass('story-loading');
-	}
-
-	//append styles to the active story tile
-	$('.story-tile').removeClass('active').addClass('inactive');
-	
-	if ( clicked.hasClass('story-tile') ) {
-		clicked.removeClass('inactive').addClass('active');
-	}
-	
-	//transition to story-loading state			
-	$('#active-story').addClass('story-loading');
-	$('body').addClass('story-loading');
-
-	//console.log( 'sending:' + clicked.attr('async-source') );
-	//console.log( $('#collection-single').attr('async-source') );
-
-	$.post( async.url, {
-			'action': 'cm_get_story',
-			'story': clicked.attr('async-source'),
-			'collection': $('#collection-single').attr('async-source')
-		}, update_dom_contents
-	)
-	.done( cleanup_async_call )
-	.fail(function(){console.log('done with failure');});	
-}
-
-function loadInitialStory() {
-	var target = $('#collection-single').attr('async-trigger');
-
-	if ( target ) {
-		storyToggle( $( '#story-' + target ) );
-	}
-}
-
-
-function update_dom_contents( quote ) {
-	try {
-		var data = $.parseJSON( quote );
-			//console.log( data );
-
-		if ( data.success ) {
-			//console.log( "success" );
-
-			var story = $('[async-target="story"]');
-			var prev = $('[async-target="previous"]');
-			var curr = $('[async-target="current"]');
-			var next = $('[async-target="next"]');
-
-			prev.text( data.previous_story.title );
-			prev.closest('a.story-toggle').attr( 'async-source', data.previous_story.id );
-
-			curr.text( data.story_title );
-
-			next.text( data.next_story.title  );
-			next.closest('a.story-toggle').attr( 'async-source', data.next_story.id );
-
-			story.html( data.post );
-			//videoSetup();
-
-
-		} else {
-
-			$('[async-target="story"]').html( data.error );
-
-		}
-
-	} catch (e) {
-		console.log( "error" );
-		console.log( e );
-	}
-}
-
-function cleanup_async_call() {
-	// moved the animation to after the dom has loaded, because of a bottleneck on the event thread
-	$('html,body').animate({scrollTop: 0},1000);	
-	$('#active-story').animate({scrollTop: 0},1000);
-
-	ch = $(window).height();
-	cw = $(window).width();
-	heroHeight = cw/3;
-	storyHeight = ch - 110;
-
-	setTimeout( function() { // is this needed?
-		storyHeight = $(window).height() - 110;
-
-		$('#active-story').addClass('story-activated');
-		$('body').addClass('story-activated').addClass('story-active');	
-		$('#active-story').height(storyHeight);		
-		$('#active-story').removeClass('story-loading').addClass('story-loaded');
-		$('body').removeClass('story-loading').addClass('story-loaded');
-
-		flexsliderSetup();
-		videoSetup();
-		storySetup();
-		view();
-
-	}, 1500 ); // is this needed?
-
-}
-
-
-
-function playVideo(){
-
-	$('body').addClass('story-video-active');
-	view();
-	
-}
 
 //measure, resize, and adjust the viewport
 function view(){
@@ -359,26 +158,6 @@ function view(){
 	fw = cw*.5;
 	storyHeight = cw/3;
 	storyVideoHeight = ch - 110;
-
-
-	if($('.story').hasClass('story-type-video')){
-
-		if($('body').hasClass('story-video-active')){
-			$('.story-hero').css('height',storyVideoHeight);
-			$('.story-video-container').css('height',storyVideoHeight);
-		}
-		else{
-			$('.story-hero').css('height',storyHeight);
-			$('.story-video-container').css('height',storyVideoHeight);		
-		}
-	}
-	else if($('.story').hasClass('story-type-video-gallery')){	
-		$('.story-hero').css('height','auto');
-		$('.video-gallery-main').css('height',ch/2);
-	}	
-	else{
-		$('.story-hero').css('height',storyHeight);
-	}	
 	
 	if($(window).width() >= 768){		
 
@@ -418,14 +197,6 @@ function loadPage(){
 	loaded = true;
 	
 	flexsliderSetup();
-	var duration;
-
-	if($('body').hasClass('home')){
-		duration = 1000;
-	}
-	else{
-		duration = 750;
-	}
 
 	setTimeout(function(){
 		$('.loading').addClass('loaded');
@@ -455,47 +226,13 @@ $(window).scroll(function() {
 
 	}
 
-	console.log($(this).scrollTop());
-	if( $('body').hasClass('story-active') && $(this).scrollTop() > 0) {
-		$('body').removeClass('story-active').addClass('story-removed');
-	}
-	else if( $('body').hasClass('story-removed') && $(this).scrollTop() <= 0){
-		$('body').addClass('story-active').removeClass('story-removed');
-	}
-
-	if( $(this).scrollTop() > 60) {
-		$('.menu-minimal').removeClass('visible-xs');
-		$('.menu-full').addClass('hidden');
-	}
-	else if( $(this).scrollTop() < 60) {
-		$('.menu-minimal').addClass('visible-xs');
-		$('.menu-full').removeClass('hidden');
-	}
-
 });//end window.scroll
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 $(document).on('spy-init', function() {
 
 	var current = undefined;
 	var previousBodyClass;
-
 
 	spied = {};
 
@@ -576,34 +313,4 @@ $(document).on('spy-init', function() {
 	 	//return ($('body').hasClass('home')) ? ((w.width() < 768) ? 350 : (w.height() / 2)) : 80;
 	 }
 });
-
-function videoSetup() {
-	var players = [];
-
-	var iframe = $('#story-video-1');
-
-	console.log( iframe );
-
-	if ( !$.isEmptyObject( iframe ) ) {
-		iframe.each( function( undefined,frame ) {
-			var p = players[ $( frame ).attr('id') ] = $f( frame );
-
-			p.addEvent('ready', function( player_id ) {
-				 console.log('ready');
-				 p.addEvent('play', function(d){});
-			});
-
-			$('.story-video-play').bind('click', function() {
-		    		p.api('play');
-			});
-		});
-	}	
-}
-
-
-function scrollStart(){
-	$('body').css('background-color','red');
-}
-
-
 
